5c6e8351d56981cb4a3c4c70239bf510ac20f8be
Adam Nnoli <aon2@cornell.edu>
2020-05-05 13:26:53 -0400

    Stubbed in Class Definitions

--- objects.py
+++ objects.py
@@ -1,3 +1,31 @@
-class GameConstants:
+class gameConstants:
   numPlayers = 4
   startingCash = 1500
+
+class boardTile:
+  pass
+
+class player:
+  pass
+
+class communityChestCard:
+  def __init__(self, text, action):
+    self._text = text 
+    self._action = action 
+  
+  def getText(self):
+    self._text 
+  
+  def getAction(self): 
+    self._action 
+
+class chanceCard:
+  def __init__(self, text, action):
+    self._text = text 
+    self._action = action 
+  
+  def getText(self):
+    self._text 
+  
+  def getAction(self): 
+    self._action
