632c4ca2a9326460737b693f43bad3d96b14efab
Adam <aon2@cornell.edu>
2020-05-20 16:46:19 -0400

    Made Game Board

--- app.py
+++ app.py
@@ -27,9 +27,13 @@ class Monopoly:
     # Helper functions make game board
 
     def create_board(self, root):
-        canvas = Canvas(root, bg="white")
+        canvas = Canvas(root, bg="white", width=(
+            2*long+8*short), height=(2*long+8*short))
         canvas.grid(row=0, column=0)
         self.create_top(canvas)
+        self.create_left(canvas)
+        self.create_right(canvas)
+        self.create_bottom(canvas)
 
     def create_top(self, cvs):
         cvs.create_rectangle(0, 0, long, long)
@@ -45,3 +49,60 @@ class Monopoly:
 
     def create_left(self, cvs):
         cvs.create_rectangle(0, long, long, long + short)
+        cvs.create_rectangle(0, long + short, long, long + 2 * short)
+        cvs.create_rectangle(0, long + 2 * short, long, long + 3 * short)
+        cvs.create_rectangle(0, long + 3 * short, long, long + 4 * short)
+        cvs.create_rectangle(0, long + 4 * short, long, long + 6 * short)
+        cvs.create_rectangle(0, long + 5 * short, long, long + 7 * short)
+        cvs.create_rectangle(0, long + 6 * short, long, long + 8 * short)
+
+    def create_right(self, cvs):
+        cvs.create_rectangle(long + 8 * short, long,
+                             2 * long + 8 * short, long + short)
+
+        cvs.create_rectangle(long + 8 * short, long +
+                             short, 2 * long + 8 * short, long + 2 * short)
+
+        cvs.create_rectangle(long + 8 * short, long + 2 *
+                             short, 2 * long + 8 * short, long + 3 * short)
+
+        cvs.create_rectangle(long + 8 * short, long + 3 *
+                             short, 2 * long + 8 * short, long + 4 * short)
+
+        cvs.create_rectangle(long + 8 * short, long + 4 *
+                             short, 2 * long + 8 * short, long + 6 * short)
+
+        cvs.create_rectangle(long + 8 * short, long + 5 *
+                             short, 2 * long + 8 * short, long + 7 * short)
+
+        cvs.create_rectangle(long + 8 * short, long + 6 *
+                             short, 2 * long + 8 * short, long + 8 * short)
+
+    def create_bottom(self, cvs):
+        cvs.create_rectangle(0, long + 8 * short, long, 2 * long + 8 * short)
+
+        cvs.create_rectangle(long, long + 8 * short,
+                             long + short, 2 * long + 8 * short)
+
+        cvs.create_rectangle(long + short, long + 8 * short, long + 2 *
+                             short, 2 * long + 8 * short)
+
+        cvs.create_rectangle(long + 2 * short, long + 8 *
+                             short, long + 3 * short, 2 * long + 8 * short)
+
+        cvs.create_rectangle(long + 3 * short, long + 8 *
+                             short, long + 4 * short, 2 * long + 8 * short)
+
+        cvs.create_rectangle(long + 4 * short, long + 8 *
+                             short, long + 5 * short, 2 * long + 8 * short)
+
+        cvs.create_rectangle(long + 5 * short, long + 8 *
+                             short, long + 6 * short, 2 * long + 8 * short)
+
+        cvs.create_rectangle(long + 6 * short, long + 8 *
+                             short, long + 8 * short, 2 * long + 8 * short)
+
+        cvs.create_rectangle(long + 7 * short, long + 8 *
+                             short, long + 8 * short, 2 * long + 8 * short)
+        cvs.create_rectangle(long + 8 * short, long + 8 *
+                             short, 2 * long + 8 * short, 2 * long + 8 * short)
